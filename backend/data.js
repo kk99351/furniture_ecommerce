a20d4cd514da2173e6105111edfd8fd83612e8e6
kundan <kk99351@gmail.com>
2022-04-06 13:53:58 +0530

    mongo db

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+    users: [
+        {
+            name: 'Basir',
+            email: 'kk99351@gmail.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+        {
+            name: 'Kundan',
+            email: 'kk9935@gmail.com',
+            password: bcrypt.hashSync('2345', 8),
+            isAdmin: false,
+        },
+    ],
     products:[
         {
          _id:'1',
