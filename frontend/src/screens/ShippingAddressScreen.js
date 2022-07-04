fd68ed14ea7b11b375f6b6acc006515e8b4ac057
kundan <kk99351@gmail.com>
2022-07-04 14:34:29 +0530

    payment_done

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { saveShippingAddress } from "../actions/cartActions";
@@ -7,12 +7,14 @@ import CheckoutSteps from "../components/CheckoutSteps";
 export default function ShippingAddressScreen(props) {
     const navigate = useNavigate();
     const userSignin = useSelector((state) => state.userSignin);
-    const cart = useSelector(state => state.cart);
+    const cart = useSelector((state) => state.cart);
     const { shippingAddress } = cart;
     const {userInfo} = userSignin;
-    if (!userInfo){
-        navigate('/signin');
-    }
+    useEffect(() => {
+        if (!userInfo) {
+          navigate('/signin');
+        }
+      }, [navigate, userInfo]);
     const [fullName, setFullName] = useState(shippingAddress.fullName);
     const [address, setAddress] = useState(shippingAddress.address);
     const [city, setCity] = useState(shippingAddress.city);
